2288c38c250fedb6074efceaed3a5c8e56e1418d
Akshin013 <Akshingadimov.@gmail.com>
2025-04-28 17:07:55 +0400

    -

--- практика/app.js
+++ практика/app.js
@@ -1,51 +1,65 @@
-let  a = [2,2,3,4,5,7]
-let  b = [2,3,5,10,18]
+const endpoint = "https://jsonplaceholder.typicode.com/users"
+// const nameP = document.getElementById("name")
+const emailp = document.getElementById("email")
+const phoneP = document.getElementById("phone")
+const container = document.getElementById("container")
 
-
-
-
-// let c = a.filter(i)
-// console.log(c);
-
-
-
-let c = [i,i,i,i,i] = a
-let e = [i,i,i,i,i] = b
-
-c.forEach(i () =>{
-    if(i===e[i]){
-        console.log();
+// fetch(endpoint, {
+//     method: "GET"    
+// }).then((res) => {
+//     return res.json();
+// }).then((data) => {
+//     console.log(data);
+//     console.log(data[0].name);
+//     const a = ""
+//     data.forEach(i => {
+//         // console.log(i.name);
+//         const a = i.name
+//         // console.log(a);
         
-    }
-})
+//     container.innerHTML += `
+//     <div class="info">
+//         <p>Name:</p>
+//         <span>${i.name}</span>
 
+//         <p>Email:</p>
+//         <span id="email">${i.email}</span>
 
-// a.forEach(i => {
-//     // c.push(i)
-//     // console.log(c);
+//         <p>Phone:</p>
+//         <span id="phone">${i.phone}</span>        
+//     </div>
+//     `
     
-
-//     console.log(i);
-//     if (i===c) {
-//         console.log(i+"  "+ "ee");
-
-        
-//     }else{
-//         console.log("err");
-        
-//     }
+//     });
+//     console.log(a);
+    
+//     nameP.innerText += a
 // })
 
-// console.log(c);
 
-//  b.forEach(i => {
-//      console.log(i);
-//      if (i===a) {
-//          console.log(i+"  "+ "ee");      
-//      }else{
-//          console.log("err");      
-//      }
-//  })
+const peoples = "https://dummyjson.com/products"
 
+fetch(peoples, {
+    method: "GET"    
+}).then((res) => {
+    return res.json();
+}).then((data) => {
+ 
+    data.products.forEach(i => {
+        console.log(i.id);
 
-
+        container.innerHTML+=`
+        <div id="container2">
+                <span>ID:</span>
+                <span>${i.id}</span>
+        
+                <p>Title:</p>
+                <span>${i.title}</span>
+        
+                <p>Rating:</p>
+                <span>${i.rating}</span>    
+            </div>
+    
+        `
+    });        
+})
